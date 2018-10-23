0d9f30168268f7d3a0f561b4ad854bdf23462fe0
Augustoz1 <augusto_16_95@hotmail.com>
2018-10-23 16:40:01 -0500

    asf

--- src/components/header.vue
+++ src/components/header.vue
@@ -2,8 +2,8 @@
 <div class="container-fluid consul_head">
 <div class="row central">
 <div class="col-md-3 cab1">
-    <img src="http://www.dreancash.gob.pe/Imagenes/Minedu.png" class="img1"/>
-    <img src="http://www.dreancash.gob.pe/Imagenes/drea.png" class="img2"/>
+    <img src="http://sitaav.org/img/drea.jpg" class="img1"/>
+    <img src="http://sitaav.org/img/dgp.jpg" class="img2"/>
     <br>Dirección Regional de Educación Ancash
 </div>
 <div class="col-md-6 cab2">
